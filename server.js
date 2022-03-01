e2e1c29e36a576c11ba2f8be092b7500f712697b
Don Reeves <donjreeves@gmail.com>
2022-02-28 20:11:07 -0500

    second commit

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
 // MIDDLEWARE
 app.use(express.urlencoded({extended: true}))
 
+// Languages: 
+const languagesController = require('./controllers/languages_controller.js')
+app.use('/languages', languagesController)
+
 // ROUTES
 app.get('/', (req, res) => {
   res.send('Welcome to the Hello World! API')
@@ -21,4 +25,4 @@ app.get('/', (req, res) => {
 // LISTEN
 app.listen(PORT, () => {
   console.log('Greetings! From port: ', PORT);
-})
+}) 
